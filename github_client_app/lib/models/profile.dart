f6a08c7703361c371984d023c6430597f50cbd43
FengchenX <fengchenxiaonvsheng@hotmail.com>
2020-03-23 15:19:22 +0800

    add

--- github_client_app/lib/models/profile.dart
+++ github_client_app/lib/models/profile.dart
@@ -5,15 +5,16 @@ part 'profile.g.dart';
 
 @JsonSerializable()
 class Profile {
-    Profile();
+  Profile();
 
-    User user;
-    String token;
-    num theme;
-    CacheConfig cache;
-    String lastLogin;//最后一次注销登录的用户名
-    String locale;
-    
-    factory Profile.fromJson(Map<String,dynamic> json) => _$ProfileFromJson(json);
-    Map<String, dynamic> toJson() => _$ProfileToJson(this);
+  User user;
+  String token;
+  num theme;
+  CacheConfig cache;
+  String lastLogin; //最后一次注销登录的用户名
+  String locale;
+
+  factory Profile.fromJson(Map<String, dynamic> json) =>
+      _$ProfileFromJson(json);
+  Map<String, dynamic> toJson() => _$ProfileToJson(this);
 }
