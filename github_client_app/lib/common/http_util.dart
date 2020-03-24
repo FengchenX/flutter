218a906a8443468cb3fa4f58a64ff32ae31b9a9f
chenfeng <979924272@qq.com>
2020-03-24 11:27:33 +0800

    add

--- github_client_app/lib/common/http_util.dart
+++ github_client_app/lib/common/http_util.dart
@@ -8,20 +8,17 @@ import 'Global.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
-class Git {
+class HttpUtil {
   // 在网络请求过程中可能会需要使用当前的context信息，比如在请求失败时
   // 打开一个新路由，而打开新路由需要context信息。
   BuildContext context;
   Options _options;
   static Dio dio = new Dio(BaseOptions(
-    baseUrl: 'https://api.github.com/',
-    headers: {
-      HttpHeaders.acceptHeader: "application/vnd.github.squirrel-girl-preview,"
-          "application/vnd.github.symmetra-preview+json",
-    },
+    baseUrl: 'http:192.168.1.68:8080',
+    headers: {},
   ));
 
-  Git([this.context]) {
+  HttpUtil([this.context]) {
     _options = Options(extra: {"context": context});
   }
 
