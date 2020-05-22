4fc15c9a54bd602759a38e32a8c6a62917378258
chenfeng <979924272@qq.com>
2020-05-22 11:16:43 +0800

    add

--- feng_app/lib/main.dart
+++ feng_app/lib/main.dart
@@ -1,12 +1,9 @@
-import 'package:fengapp/test/my_app.dart';
-import 'package:flutter/material.dart';
-import 'dart:io';
-import 'package:flutter/services.dart';
 import 'package:fengapp/app/app_scene.dart';
+import 'package:flutter/material.dart';
 
 void main() {
-//  runApp(AppScene());
-  runApp(MyApp());
+  runApp(AppScene());
+//  runApp(MyApp());
 //  if (Platform.isAndroid) {
 //    SystemUiOverlayStyle systemUiOverlayStyle =
 //        SystemUiOverlayStyle(statusBarColor: Colors.transparent);
